72407e209149bb83246d31afd2c4a52d4d4bf03c
Shubham18598 <muddamwarshubham98@gmail.com>
2024-05-22 15:32:42 +0530

    add filter

--- src/Expense.js
+++ src/Expense.js
@@ -1,60 +1,65 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const Expense = () => {
-  const [transactions, setTransactions] = useState([])
-  const [text, setText] = useState("")
-  const [amount, setAmount] = useState("")
-
-  useEffect(() => {
-    const localStorageTransactions = JSON.parse(
-      localStorage.getItem("expense-tracker")
-    )
-    if (localStorageTransactions) {
-      setTransactions(localStorageTransactions)
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem("expense-tracker", JSON.stringify(transactions))
-  }, [transactions])
+  const [transactions, setTransactions] = useState([]);
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("");
+  const [filterOption, setFilterOption] = useState("all");
 
   const addTransaction = (type) => {
     if (text.trim() === "" || amount.trim() === "") {
-      alert("Please add a text and amount")
-      return
+      alert("Please add a text and amount");
+      return;
     }
 
     const newTransaction = {
-      id: generateID(),
+      id: uuidv4(),
       text,
       amount: type === "income" ? +amount : -amount,
-    }
+    };
 
-    setTransactions([...transactions, newTransaction])
-    setText("")
-    setAmount("")
-  }
-
-  const generateID = () => {
-    return Math.floor(Math.random() * 100000000)
-  }
+    setTransactions([...transactions, newTransaction]);
+    setText("");
+    setAmount("");
+  };
 
   const deleteTransaction = (id) => {
-    setTransactions(transactions.filter((transaction) => transaction.id !== id))
-  }
+    setTransactions(
+      transactions.filter((transaction) => transaction.id !== id)
+    );
+  };
 
-  const total = transactions
-    .reduce((acc, transaction) => acc + transaction.amount, 0)
-    .toFixed(2)
-  const income = transactions
-    .filter((transaction) => transaction.amount > 0)
-    .reduce((acc, transaction) => acc + transaction.amount, 0)
-    .toFixed(2)
-  const expense = (
-    transactions
-      .filter((transaction) => transaction.amount < 0)
-      .reduce((acc, transaction) => acc + transaction.amount, 0) * -1
-  ).toFixed(2)
+  const filteredTransactions = () => {
+    switch (filterOption) {
+      case "all":
+        return transactions;
+      case "income":
+        return transactions.filter((transaction) => transaction.amount > 0);
+      case "expense":
+        return transactions.filter((transaction) => transaction.amount < 0);
+      default:
+        return transactions;
+    }
+  };
+
+  // Calculate total, income, and expenses
+  let total = 0;
+  let income = 0;
+  let expense = 0;
+
+  transactions.forEach((transaction) => {
+    total += transaction.amount;
+    if (transaction.amount > 0) {
+      income += transaction.amount;
+    } else {
+      expense += transaction.amount;
+    }
+  });
+
+  total = total.toFixed(2);
+  income = income.toFixed(2);
+  expense = Math.abs(expense).toFixed(2);
 
   return (
     <div className="container">
@@ -72,64 +77,81 @@ const Expense = () => {
       <div className="inc-exp-container">
         <div>
           <h4>Balance</h4>
-          <p className="money plus">${total}</p>
+          <p className="money total">${total}</p>
         </div>
       </div>
-      <h3>Add Transaction</h3>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-        }}
-      >
-        <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input
-            type="text"
-            id="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="amount">Amount</label>
-          <input
-            type="number"
-            id="amount"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            placeholder="Enter amount..."
-          />
-        </div>
-        <div className="buttons">
-          <button className="btn" onClick={() => addTransaction("income")}>
-            Add Income
-          </button>
-          <button className="btn" onClick={() => addTransaction("expense")}>
-            Add Expense
-          </button>
-        </div>
-      </form>
-
-      <h3>Transaction History</h3>
-      <ul className="list">
-        {transactions.map((transaction) => (
-          <li
-            key={transaction.id}
-            className={transaction.amount < 0 ? "minus" : "plus"}
+      <div className="add-tran">
+        <div className="add-transaction">
+          <h3>Add Transaction</h3>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
           >
-            {transaction.text} <span>${Math.abs(transaction.amount)}</span>
-            <button
-              className="delete-btn"
-              onClick={() => deleteTransaction(transaction.id)}
+            <div className="form-control">
+              <label htmlFor="text">Text</label>
+              <input
+                type="text"
+                id="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Enter text..."
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="amount">Amount</label>
+              <input
+                type="number"
+                id="amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Enter amount..."
+              />
+            </div>
+            <div className="buttons">
+              <button className="btn" onClick={() => addTransaction("income")}>
+                Income
+              </button>
+              <button className="btn" onClick={() => addTransaction("expense")}>
+                Expense
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="transaction-history">
+          <h3>Transaction History</h3>
+          <div className="select-container">
+            <select
+              className="select"
+              value={filterOption}
+              onChange={(e) => setFilterOption(e.target.value)}
             >
-              x
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+              <option value="all">All</option>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+          </div>
 
-export default Expense
+          <ul className="list">
+            {filteredTransactions().map((transaction) => (
+              <li
+                key={transaction.id}
+                className={transaction.amount < 0 ? "minus" : "plus"}
+              >
+                {transaction.text} <span>${Math.abs(transaction.amount)}</span>
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteTransaction(transaction.id)}
+                >
+                  x
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Expense;
